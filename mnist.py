a230c62a0c8d45fd2814a888291af8a2a73cf376
dianlujitao <dianlujitao@gmail.com>
2019-03-20 19:44:51 +0800

    Switch to liblinear SVM implementation
     * sklearn is slooooooow
     * Match dataset size with official demo setting

--- mnist.py
+++ mnist.py
@@ -1,25 +1,26 @@
 import numpy as np
 from torchvision.datasets import MNIST
-from sklearn.svm import SVC
+from liblinear import liblinearutil
 from pcanet import PCANet
 
-# load data
+# load data (10000 training, 50000 testing)
+n_train = 10000
+n_test = 50000
 train_set = MNIST('datasets', download=True)
-train_set_X = train_set.data[:5000].float().div(255)
-train_set_y = train_set.targets[:5000]
-test_set_X = train_set.data[5000:5100].float().div(255)
-test_set_y = train_set.targets[5000:5100]
+train_set_X = train_set.data[:n_train].float().div(255.0)
+train_set_y = train_set.targets[:n_train]
+test_set_X = train_set.data[n_train:n_train + n_test].float().div(255.0)
+test_set_y = train_set.targets[n_train:n_train + n_test]
 
 print(' ====== PCANet Training ======= ')
 net = PCANet([8, 8], 7, 7, 0.5)
 train_features = net.extract_features(train_set_X)
 
 print(' ====== Training Linear SVM Classifier ======= ')
-svm = SVC(kernel='linear')
-svm.fit(train_features, train_set_y)
+model = liblinearutil.train(
+    train_set_y.numpy(), train_features.numpy(), '-s 1 -q')
 
 print(' ====== PCANet Testing ======= ')
 test_features = net.extract_features(test_set_X, False)
-test_set_pred = svm.predict(test_features)
-diff = np.sum(test_set_y.numpy() != test_set_pred)
-print('Test error: %f' % (diff / len(test_set_y)))
+pred_labels, (acc, mse, scc), pred_values = liblinearutil.predict(
+    test_set_y.numpy(), test_features.numpy(), model)
